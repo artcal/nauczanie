7fc41f096b115d73654e11e1904e7d79bbe42370
artcal <arturcaluch@gmail.com>
2018-10-14 17:12:09 +0200

    Add files via upload

---
+++ title_case_a_sentence.js
@@ -0,0 +1,7 @@
+function titleCase(str) {
+ var arr = str.toLowerCase().split(" ");
+ for(var i = 0; i < arr.length; i++){
+   arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+ }
+ return arr.join(" ");
+}
